53f5d03c4368de9e8d326ffd76477cf84d067f75
Konstantin Kolesnyak <constkolesnyak@gmail.com>
2023-08-23 05:44:59 +0600

    Finish refactoring config.py

--- src/cstow/config.py
+++ src/cstow/config.py
@@ -2,11 +2,9 @@ import copy
 import itertools
 import os
 import tomllib
-from functools import partial
 from string import Template
-from typing import Annotated, Iterator, Optional, Self
+from typing import Annotated, Iterator, Self
 
-import attrs
 import pydantic as pd
 from path import Path
 
@@ -76,30 +74,43 @@ TarsDirs = dict[DirectoryPath, list[DirectoryPath]]
 
 
 class Config(pd.BaseModel, extra='forbid'):
-    cmd_template: CmdTemplate
+    cmd_template: CmdTemplate = Template(COMMAND_TEMPLATE_DEFAULT)  # type: ignore
+    root_dir: DirectoryPath = '/'  # type: ignore
+    targets_dirs: TarsDirs
 
+    @pd.field_validator('targets_dirs', mode='before')
+    @classmethod
+    def _(cls, targets_dirs: RawTarsDirs, info: pd.FieldValidationInfo) -> RawTarsDirs:
+        targets_dirs = copy.deepcopy(targets_dirs)
 
-class _OldRawConfig(pd.BaseModel, extra='forbid'):
-    cmd_template: str = COMMAND_TEMPLATE_DEFAULT
-    root_dir: str = Path('/')
-    targets_dirs: TarsDirsStr
+        assert 'root_dir' in info.data, "'root_dir' is invalid"
+        root_dir = info.data['root_dir']
 
-    config_path: str
+        assert isinstance(targets_dirs, dict), "'targets_dirs' must be a table"
+        for target, dirs in targets_dirs.items():
+            assert isinstance(dirs, list), "'dirs' must be an array"
+            targets_dirs[target] = [root_dir / _expand(dir_) for dir_ in dirs]
+
+        return targets_dirs
 
     @classmethod
-    def from_env_var(cls, env_var: str) -> "_OldRawConfig":
+    def from_env_var(cls, env_var: str = _CONFIG_PATH_ENV_VAR) -> Self:
         return cls.from_path(_env_var_to_path(env_var))
 
     @classmethod
-    def from_path(cls, path: Path) -> "_OldRawConfig":
+    def from_path(cls, path: Path) -> Self:
         try:
-            return cls(**tomllib.loads(path.read_text()), config_path=path)
+            return cls(**tomllib.loads(path.read_text()))
         except FileNotFoundError:
             raise ConfigNotFoundError(path)
-        except (tomllib.TOMLDecodeError, pd.ValidationError) as e:
+        except tomllib.TOMLDecodeError as e:
             raise InvalidConfigError(path, str(e))
-        except TypeError:
-            raise InvalidConfigError(path, 'config_path\n  Extra keys are not permitted')
+        except pd.ValidationError as e:
+            raise InvalidConfigError.from_pydantic(path, e)
+
+    def each_target_and_dir(self) -> Iterator[tuple[DirectoryPath, DirectoryPath]]:
+        for target, dirs in self.targets_dirs.items():
+            yield from itertools.product((target,), dirs)
 
 
 def _env_var_to_path(env_var: str) -> Path:
@@ -107,47 +118,3 @@ def _env_var_to_path(env_var: str) -> Path:
         return Path(os.environ[env_var])
     except KeyError:
         raise ConfigEnvVarUnsetError(env_var)
-
-
-@attrs.define
-class OldConfig:
-    cmd_template: Template
-    targets_dirs: TarsDirsPath = attrs.field(converter=copy.deepcopy)  # type: ignore
-    _root_dir: Path
-
-    @classmethod
-    def _from_raw_config(cls, raw_cfg: _OldRawConfig) -> "OldConfig":
-        cmd_template: Optional[Template] = _validate_cmd_template(raw_cfg.cmd_template)
-        if cmd_template is None:
-            raise InvalidConfigError(
-                raw_cfg.config_path,
-                f"'cmd_template' is invalid:\n{raw_cfg.cmd_template}",
-            )
-
-        # todo FileNotFound -> InvalidConfig exceptions
-        root_dir: Path = _str_to_dir(raw_cfg.root_dir)
-        targets_dirs: TarsDirsPath = _tds_to_tdp(raw_cfg.targets_dirs, root_dir)
-
-        return cls(cmd_template, targets_dirs, root_dir)
-
-    @classmethod
-    def from_env_var(cls, env_var: str = _CONFIG_PATH_ENV_VAR) -> "OldConfig":
-        return OldConfig._from_raw_config(_OldRawConfig.from_env_var(env_var))
-
-    def each_target_and_dir(self) -> Iterator[tuple[Path, Path]]:
-        for target, dirs in self.targets_dirs.items():
-            yield from itertools.product((target,), dirs)
-
-
-def _tds_to_tdp(targets_dirs: TarsDirsStr, root_dir: Path) -> TarsDirsPath:
-    return {
-        _str_to_dir(target): list(map(partial(_str_to_dir, root_dir=root_dir), dirs))
-        for target, dirs in targets_dirs.items()
-    }
-
-
-def _str_to_dir(path_str: str, root_dir: Path = Path('/')) -> Path:
-    path: Path = root_dir / Path(path_str).expand()
-    if path.isdir():
-        return path
-    raise FileNotFoundError(f"Directory '{path}' is not found")
