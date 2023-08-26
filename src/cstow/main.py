34096bfe43ccee1e9d518bc454a0d6821436e46d
Konstantin Kolesnyak <constkolesnyak@gmail.com>
2023-08-26 19:38:27 +0600

    main: Add help page

--- src/cstow/main.py
+++ src/cstow/main.py
@@ -11,6 +11,18 @@ from cstow.view import PlainView, RichView, View
 _ACTION_DEFAULT = CmdAction.NO
 
 
+def _help() -> str:
+    '''Construct the help page. Let Fire generate boilerplate.'''
+    actions = ', '.join(CmdAction).replace(_ACTION_DEFAULT, _ACTION_DEFAULT + ' (default)')
+
+    return f'''
+    https://github.com/constkolesnyak/cstow/blob/main/README.md
+
+    Args:
+        action: Actions: {actions}. 
+        plain: Print plain text only. 
+    '''
+
 
 def _error(*args: Any, **kwargs: Any) -> NoReturn:
     '''Print the error(s) and exit with 1.'''
@@ -32,6 +44,7 @@ def _cli(action: str = _ACTION_DEFAULT, /, *, plain: bool = False) -> None:
 
 def main() -> None:
     '''The entry point.'''
+    _cli.__doc__ = _help()
     fire.Fire(_cli)  # type: ignore
 
 
