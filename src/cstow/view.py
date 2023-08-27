4e1d601891a6cc3bfc4765f19c9c59a94189afca
Konstantin Kolesnyak <constkolesnyak@gmail.com>
2023-08-27 17:35:35 +0600

    Implement RichView

--- src/cstow/view.py
+++ src/cstow/view.py
@@ -1,8 +1,12 @@
+import os
 import sys
 from abc import ABC, abstractmethod
 from subprocess import CompletedProcess
 
 from path import Path
+from rich.console import Console
+from rich.highlighter import RegexHighlighter
+from rich.theme import Theme
 
 from cstow.command import CmdAction
 
@@ -39,17 +43,39 @@ class PlainView(View):
             print(stderr, file=sys.stderr)
 
 
+class _StowHighlighter(RegexHighlighter):
+    """The highlighter for GNU Stow output."""
+
+    highlights = [r'(?P<linking>(UN)?LINK)', r'(?P<linking>=>)']
+    theme = Theme({"linking": "bold spring_green4"})
+
+
 class RichView(View):
     '''The view for printing rich text.'''
 
+    def __init__(self) -> None:
+        self.console = Console(highlighter=_StowHighlighter(), theme=_StowHighlighter.theme)
+        self.error_console = Console(stderr=True, style="red")
+
     def show_action(self, action: CmdAction) -> None:
-        ...
+        surround = '[deep_pink3]' + '@' * 15
+
+        self.console.print(
+            f'{surround}   [bold green]{_action_to_title(action)}   {surround}\n',
+            justify='center',
+        )
 
     def show_dir(self, dir_: Path) -> None:
-        ...
+        self.console.print(
+            f"[cyan]{dir_.parent}{os.path.sep}[yellow bold]{dir_.name}\n", justify='center'
+        )
 
     def show_proc(self, proc: CompletedProcess[bytes]) -> None:
-        ...
+        if stdout := proc.stdout.decode():
+            self.console.print(stdout)
+        if stderr := proc.stderr.decode():
+            self.error_console.print('[bold]ERROR:\n')
+            self.error_console.print(stderr)
 
 
 def _action_to_title(action: CmdAction) -> str:
